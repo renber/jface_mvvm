fb874f38c0c99f8af6d6a3f05ac98c4584f029db
renber <rene.bergelt@informatik.tu-chemnitz.de>
2020-03-16 15:25:52 +0100

    extended documentation comment for ItemTransformer

--- src/main/java/de/renber/databinding/collections/ItemTransformer.java
+++ src/main/java/de/renber/databinding/collections/ItemTransformer.java
@@ -4,8 +4,8 @@ package de.renber.databinding.collections;
  * Interface for methods which transform one object type into another
  * @author renber
  *
- * @param <TIn>
- * @param <TOut>
+ * @param <TIn> Source type
+ * @param <TOut> Target type, source type is transformed to
  */
 public interface ItemTransformer<TIn, TOut> {
 
